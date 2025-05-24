9f407f874ff06c5c0d49d6dba49c20ac0d6ca2b3
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2025-05-24 16:57:12 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const tr = table.querySelectorAll('tr');
+
+for (let i = 0; i < tr.length; i++) {
+  const td = document.createElement('td');
+
+  td.textContent = tr[i].children[1].textContent;
+
+  tr[i].insertBefore(td, tr[i].children[tr[i].children.length - 1]);
+}
