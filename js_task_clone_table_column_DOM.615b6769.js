8ec3d1f987069332c3dc70977788948d6a4c7098
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2025-05-24 16:57:58 +0300

    make build

---
+++ js_task_clone_table_column_DOM.615b6769.js
@@ -0,0 +1,2 @@
+"use strict";for(var table=document.querySelector("table"),tr=table.querySelectorAll("tr"),i=0;i<tr.length;i++){var t=document.createElement("td");t.textContent=tr[i].children[1].textContent,tr[i].insertBefore(t,tr[i].children[tr[i].children.length-1])}
+//# sourceMappingURL=js_task_clone_table_column_DOM.615b6769.js.map
---
+++ js_task_clone_table_column_DOM.f75de5e1.js
@@ -0,0 +1,10 @@
+'use strict';
+const table = document.querySelector('table');
+const tr = table.querySelectorAll('tr');
+for(let i = 0; i < tr.length; i++){
+    const td = document.createElement('td');
+    td.textContent = tr[i].children[1].textContent;
+    tr[i].insertBefore(td, tr[i].children[tr[i].children.length - 1]);
+}
+
+//# sourceMappingURL=js_task_clone_table_column_DOM.f75de5e1.js.map
